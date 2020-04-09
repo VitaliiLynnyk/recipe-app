6ba976b151e36e40cc46557e98407ea3b4366641
VitaliiLynnyk <linnik2101@gmail.com>
2020-04-09 12:04:15 +0200

    add ingredient leftJoinAndSelect to recipe

--- src/recipe/recipe.repository.ts
+++ src/recipe/recipe.repository.ts
@@ -13,7 +13,7 @@ export class RecipeRepository extends Repository<Recipe> {
 
   async getRecipes(getFilterRecipeDto: GetFilterRecipeDto): Promise<Recipe[]> {
     const { search } = getFilterRecipeDto;
-    const query = this.createQueryBuilder('recipe').leftJoinAndSelect('recipe.recipeIngredients', 'recipeIngredient');
+    const query = this.createQueryBuilder('recipe').leftJoinAndSelect('recipe.recipeIngredients', 'recipeIngredient').leftJoinAndSelect('recipeIngredient.ingredient', 'ingredient');
 
     if (search) {
       query.andWhere('(recipe.name LIKE :search OR recipe.description LIKE :search)', { search: `%${search}%` });
