0dab551816400d19887ff1cbff84c14a0023ee4c
VitaliiLynnyk <linnik2101@gmail.com>
2020-04-14 15:32:01 +0200

    add SignUp validation

--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,5 +1,24 @@
+import { IsString, MinLength, MaxLength, IsNotEmpty, IsEmail, Matches } from "class-validator";
+
 export class AuthCredentialsDto {
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(4)
+  @MaxLength(20)
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(
+    /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
+    { message: 'password too weak' }
+  )
   password: string;
 }
