74d525b2cbac18cb6e70bde471186d0ac86e8784
VitaliiLynnyk <linnik2101@gmail.com>
2020-04-14 15:14:30 +0200

    init auth module with all settings

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,20 +3,22 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { typeOrmConfig } from './config/typeorm.config';
 
+import { AuthModule } from './auth/auth.module';
 import { RecipeModule } from './recipe/recipe.module';
-import { IngredientModule } from './ingredient/ingredient.module';
 import { NutritionModule } from './nutrition/nutrition.module';
-import { RecipeIngredientModule } from './recipe-ingredient/recipe-ingredient.module';
+import { IngredientModule } from './ingredient/ingredient.module';
 import { RecipeNutritionModule } from './recipe-nutrition/recipe-nutrition.module';
+import { RecipeIngredientModule } from './recipe-ingredient/recipe-ingredient.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(typeOrmConfig),
+    AuthModule,
     RecipeModule,
     NutritionModule,
     IngredientModule,
     RecipeIngredientModule,
-    RecipeNutritionModule,
+    RecipeNutritionModule
   ],
   controllers: [],
   providers: [],
